f803cabf3267b4fd52068bce6a175e4a757c8242
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-01-26 12:24:51 +0100

    change retro md

--- retrospective.md
+++ retrospective.md
@@ -1,4 +1,4 @@
-[![Retrospective - Smart_Is_Beautiful_Project](https://img.shields.io/static/v1?label=Gaudenz77&message=Smart_Is_Beautiful_Project&color=eb6b34&style=for-the-badge)](https://github.com/Gaudenz77/Smart_Is_Beautiful_Project)
+[![Gaudenz77 - Retrospective - Smart_Is_Beautiful_Project](https://img.shields.io/badge/Gaudenz77_--_Retrospective-Smart__Is__Beautiful__Project-eb6b34?style=for-the-badge)](https://github.com/Gaudenz77/Smart_Is_Beautiful_Project/blob/main/retrospective.md)
 ---
 # Retrospective
 ---
--- tools/workbench.php
+++ tools/workbench.php
@@ -14,4 +14,12 @@ _Dash badge_
 _Query parameter badge_
 
 [![Gaudenz77 - Smart_Is_Beautiful_Project](https://img.shields.io/static/v1?label=Gaudenz77&message=Smart_Is_Beautiful_Project&color=eb6b34&style=for-the-badge)](https://github.com/Gaudenz77/Smart_Is_Beautiful_Project)
- <!-- Badge Dash -->     
+ <!-- Badge Dash Retrospective -->   
+ _Dash badge_
+
+[![Gaudenz77 - Retrospective - Smart_Is_Beautiful_Project](https://img.shields.io/badge/Gaudenz77_--_Retrospective-Smart__Is__Beautiful__Project-eb6b34?style=for-the-badge)](https://github.com/Gaudenz77/Smart_Is_Beautiful_Project/blob/main/retrospective.md)
+
+_Query parameter badge_
+
+[![Gaudenz77 - Retrospective - Smart_Is_Beautiful_Project](https://img.shields.io/static/v1?label=Gaudenz77+-+Retrospective&message=Smart_Is_Beautiful_Project&color=eb6b34&style=for-the-badge)](https://github.com/Gaudenz77/Smart_Is_Beautiful_Project/blob/main/retrospective.md)
+        
