c60cdf6f183e66a52d4202a426795226d20df6c4
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-01-28 13:57:17 +0100

    approved changes

---
+++ includes/db_connect.php
@@ -0,0 +1,9 @@
+<?php
+// Connect to the database
+$host = "hostname";
+$dbname = "database_name";
+$username = "username";
+$password = "password";
+
+// $dbConnection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+?>
--- index.php
+++ index.php
@@ -11,6 +11,7 @@ require "./includes/header.php";
     <div class="col bg-warning m-4 p-4">
       <h1 class="display-2"><br>Smart Is Beautiful ... Are You?</h1>
       <h3>Dear Smartypants: Chose A Topic:</h3>
+      <form action="" method="post">
       <p>
         <select class="form-control" name="quiz" id="quiz">
           <option value="1">Astronautics - Space-Travel</option>
@@ -26,6 +27,9 @@ require "./includes/header.php";
 <?php
 require "./includes/footer.php";
 ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</form>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>
