b622bfbd8e194c727a6ed3a1c09661c3ffe414ce
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-01-30 08:07:02 +0100

    deactivated animate.css

--- includes/html_head_tag.php
+++ includes/html_head_tag.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="stylesheet" href="./css/mystyle.css">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+   <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -18,12 +18,11 @@
       body {
         font-weight:bold;
         font-family: 'Oxygen', sans-serif;
-        margin-top: 200px;
-        
-      }
+        margin-top: 200px;  
+        }
 
       #errorMessage {
-    color: rgb(106, 177, 0);
-}
+        color: rgb(106, 177, 0);
+        }
     </style>
 </head>
